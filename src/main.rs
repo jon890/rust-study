05516d3841d2ce3a94156b15ba1681ce11030fe1
BiFoS <jon89071@gmail.com>
2023-08-01 11:44:01 +0900

    :memo: Comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+/// 책으로 쓰는 타입 - documentation
+struct Book;
+
+fn main() { /* */
+    let x /*: i16 */ = 10;
+
+    let _my_number /*: i16 */ = 20;
+
+    let 내숫자 = 30;
+
+    // 이것이 코멘트 입니다
     println!("Hello, world!");
 }
