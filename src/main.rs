68dee64d8207d56d6d4a72d632672de0b943ea9b
BiFoS <jon89071@gmail.com>
2023-08-01 11:50:14 +0900

    :art: Number Types

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-/// 책으로 쓰는 타입 - documentation
-struct Book;
-
-fn main() { /* */
-    let x /*: i16 */ = 10;
-
-    let _my_number /*: i16 */ = 20;
-
-    let 내숫자 = 30;
-
-    // 이것이 코멘트 입니다
-    println!("Hello, world!");
+fn main() { 
+    let my_number = 100; // 255
+    let my_number2:u8 = 200; // 
+    let third_number = my_number + my_number2;
+    // u8 + i32는 경고
+    // type inference
 }
