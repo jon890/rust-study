008808c1d20ff1494addebbdf2cdf888f583f813
BiFoS <jon89071@gmail.com>
2023-08-01 13:45:53 +0900

    :memo: Character Types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() { 
-    let my_number = 100; // 255
-    let my_number2:u8 = 200; // 
-    let third_number = my_number + my_number2;
-    // u8 + i32는 경고
-    // type inference
+    println!("Hello, world!");
+    let first_letter = 'A';
+    let space = ' ';
+
+    // casting = simple type change using 'as'
+    let my_number: u16 = 8;
+    let second_number: u8 = 10;
+    let third_number = my_number + second_number as u16;
+
 }
